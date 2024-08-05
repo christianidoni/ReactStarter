d91034915a20851e22b9697b5a6768a9d5c7844f
Christian Idoni <christianidoni@gmail.com>
2024-08-05 19:23:16 +0300

    Added navigation bar, logo in Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,34 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import logo from "./logo.png";
+import "./Navigation.css";
+
+const CustomAppBar = styled(AppBar)({
+  backgroundColor: "black",
+});
+
+const StyledButton = styled(Button)({
+  "&:hover": {
+    color: "red",
+  },
+});
+
+const Navigation = () => {
+  return (
+    <CustomAppBar position="static">
+      <Toolbar>
+        <img src={logo} alt="Logo" className="logo" /> {}
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+        ></Typography>
+        <StyledButton color="inherit"> Home </StyledButton>
+        <StyledButton color="inherit"> TV Shows </StyledButton>
+        <StyledButton color="inherit"> About Us </StyledButton>
+      </Toolbar>
+    </CustomAppBar>
+  );
+};
+
+export default Navigation;
